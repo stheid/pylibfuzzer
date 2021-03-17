4a34dc43d2916a1db7a1c4099d38b4fdf11b4f56
Stefan Heid <stefan.heid@upb.de>
2021-03-17 11:23:41 +0100

    :recycle:  renamed folders and completed setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='TODO',
+    name='pylibfuzzer',
     version="0.0.1",
-    packages=['TODO'],
-    author="TODO",
-    author_email="TODO",
-    description="TODO",
+    packages=find_packages(include=['pylibfuzzer', 'pylibfuzzer.*']),
+    author="Stefan Heid",
+    author_email="stefan.heid@upb.de",
+    description="Python implementation of test case generation to interface with libfuzzer which will handle the execution",
     long_description=open('README.rst').read(),
     license="GPLv3",
-    keywords="TODO",
-    url="https://github.com/stheid/ TODO",
+    keywords="libfuzzer",
+    url="https://github.com/stheid/pylibfuzzer",
     install_requires=open('requirements.txt').read(),
     classifiers=[
         "Programming Language :: Python :: 3",
